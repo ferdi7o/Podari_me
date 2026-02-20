c569184a66e6490d500560a20cf29ec4af9868d8
Ferdi Mehmed Akif <76132036+ferdi7o@users.noreply.github.com>
2026-02-20 02:41:54 +0200

    chore/created gifts models

--- gifts/models.py
+++ gifts/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from accounts.models import Profile
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Gift(models.Model):
+    TYPE_CHOICES = [
+        ('Електроника', 'Електроника'),
+        ('Дом. Потреби', 'Домашни Потреби'),
+        ('Животни', 'Животни'),
+        ('Други', 'Други'),
+    ]
+
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=400)
+    category = models.CharField(max_length=30, choices=TYPE_CHOICES)
+    image_url = models.URLField()
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='gifts')
+    tags = models.ManyToManyField(Tag, blank=True)
+    status_gift = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
